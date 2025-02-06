af7ab6d618f6b067de2370348e75c76b160323c4
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2025-02-06 18:30:33 +0100

    Retoques en el nombre

--- librepost-express/routes/about.js
+++ librepost-express/routes/about.js
@@ -5,7 +5,7 @@ const router = express.Router();
 router.get('/', (req, res) => {
     res.render('about', { user:req.session.user, 
     title: 'About us',   
-    message: 'Somos, Libre Post, una empresa dedicada a la venta de productos y servicios de calidad. ¡Conócenos!',
+    message: 'Somos, Post Libre, una empresa dedicada a la venta de productos y servicios de calidad. ¡Conócenos!',
     player: 'Invitado'});
  }); 
 
