8fdc284e01b09662de31f4cc9f026ed1fc3bc0df
Juan Antonio Peregrina Peregrina <juanantonio.peregrinaperegrina@usp.ceu.es>
2025-02-08 13:25:29 +0100

    Añadido al header librerías y rediseño de politica-cookies

--- librepost-express/routes/politica-cookies.js
+++ librepost-express/routes/politica-cookies.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("politica-cookies", { title: "Política de Cookies" });
+    res.render("politica-cookies", { title: "Política de Cookies",  user: req.session.user || { username: "Invitado" } });
 });
 
 module.exports = router;
