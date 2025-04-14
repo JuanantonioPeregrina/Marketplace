9a950c632f5ffc61b4e8a8ff3cd96ac6b5b3f4e8
Juan Antonio Peregrina <juanantonio.peregrina@gmail.com>
2025-04-14 12:23:30 +0200

    Arreglado un bug al acceder a Términos y condiciones

--- postlibre-express/routes/terminos.js
+++ postlibre-express/routes/terminos.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res) => {
-    res.render("terminos", { title: "Términos y Condiciones - LibrePost", user: req.user || null });
+    res.render("terminos", { title: "Términos y Condiciones - LibrePost", user: req.session.user || null });
 });
 
 module.exports = router;
